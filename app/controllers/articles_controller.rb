737565cb7114a4c7e23e358b31555fd299c450e1
Andreas Casillas <m221068@usna.edu>
2022-02-08 13:59:41 -0500

    Completed tutorial... i think

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+
   def index
     @articles = Article.all
   end
@@ -45,11 +47,5 @@ class ArticlesController < ApplicationController
   private
     def article_params
       params.require(:article).permit(:title, :body, :status)
-    end
-
-    http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
-
-    def index
-      @articles = Article.all
     end
 end
