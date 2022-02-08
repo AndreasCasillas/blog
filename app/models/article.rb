54aa8f86fe83ae338d3ff66c9e2714e1b414338a
Andreas Casillas <m221068@usna.edu>
2022-02-08 14:32:32 -0500

    Completed Lab 3, i think

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,9 +1,4 @@
-#class Article < ApplicationRecord
-#  has_many :comments
-#
-#  validates :title, presence: true
-#  validates :body, presence: true, length: { minimum: 10 }
-#end
+
 
 class Article < ApplicationRecord
   include Visible
