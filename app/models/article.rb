1418034e092c4d4a4d0438fb9c3da3a31fab6571
Andreas Casillas <m221068@usna.edu>
2022-02-07 21:15:20 -0500

    Finished Ch 9 move onto Ch 10

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,13 @@
+#class Article < ApplicationRecord
+#  has_many :comments
+#
+#  validates :title, presence: true
+#  validates :body, presence: true, length: { minimum: 10 }
+#end
+
 class Article < ApplicationRecord
+  include Visible
+
   has_many :comments
 
   validates :title, presence: true
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,9 @@
+#class Comment < ApplicationRecord
+#  belongs_to :article
+#end
+
 class Comment < ApplicationRecord
+  include Visible
+
   belongs_to :article
 end
